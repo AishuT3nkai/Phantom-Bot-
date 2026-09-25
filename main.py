977cd05f60bbcf74d4b28a0e7bd91f851783ddca
AishuT3nkai <zurkathelanders@gmail.com>
2026-09-25 16:41:21 +0700

    Add Phantom bot entrypoint

---
+++ main.py
@@ -0,0 +1,41 @@
+import logging
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import wavelink
+import database
+from music import Music
+
+load_dotenv()
+logging.basicConfig(level=logging.INFO)
+
+TOKEN=os.getenv("DISCORD_TOKEN")
+LAVALINK_URI=os.getenv("LAVALINK_URI","http://127.0.0.1:2333")
+LAVALINK_PASSWORD=os.getenv("LAVALINK_PASSWORD","change-this-password")
+LAVALINK_IDENTIFIER=os.getenv("LAVALINK_IDENTIFIER","PHANTOM")
+
+class Phantom(commands.Bot):
+    def __init__(self):
+        intents=discord.Intents.default()
+        intents.message_content=True
+        intents.members=True
+        super().__init__(command_prefix="!",intents=intents)
+
+    async def setup_hook(self):
+        database.init_db()
+        node=wavelink.Node(uri=LAVALINK_URI,password=LAVALINK_PASSWORD,identifier=LAVALINK_IDENTIFIER)
+        await wavelink.Pool.connect(nodes=[node],client=self,cache_capacity=100)
+        await self.add_cog(Music(self))
+        await self.tree.sync()
+
+    async def on_ready(self):
+        logging.info("Phantom online: %s (%s)",self.user,self.user.id)
+
+    async def on_wavelink_node_ready(self,payload):
+        logging.info("Lavalink ready: %s",payload.node)
+
+bot=Phantom()
+if not TOKEN:
+    raise RuntimeError("DISCORD_TOKEN is missing")
+bot.run(TOKEN)
