88d9079331f4e4bec10ffa90955cd2ab286923ea
AishuT3nkai <zurkathelanders@gmail.com>
2026-09-26 11:13:16 +0700

    Remove music bot startup code

--- main.py
+++ main.py
@@ -5,15 +5,12 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-from anti_raid import AntiRaid
-
 load_dotenv()
 
 logging.basicConfig(
     level=os.getenv("LOG_LEVEL", "INFO").upper(),
     format="%(asctime)s %(levelname)s %(name)s: %(message)s",
 )
-log = logging.getLogger("aishu")
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -23,18 +20,15 @@ class AishuBot(commands.Bot):
         intents = discord.Intents.default()
         intents.message_content = True
         intents.members = True
-        super().__init__(
-            command_prefix="!",
-            intents=intents,
-            help_command=None,
-        )
+        super().__init__(command_prefix="!", intents=intents, help_command=None)
 
     async def setup_hook(self):
-        await self.add_cog(AntiRaid(self))
         await self.tree.sync()
 
     async def on_ready(self):
-        log.info("Aishu Anti-Raid online as %s (%s)", self.user, self.user.id)
+        logging.getLogger("aishu").info(
+            "Aishu Anti-Raid online as %s (%s)", self.user, self.user.id
+        )
 
 
 bot = AishuBot()
